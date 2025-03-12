0b19cad4f42c23003efe30db86fd7ef9d09bd1f9
mirekbartunek <mucus.hooide@gmail.com>
2025-03-12 18:27:24 +0100

    added link to renting

--- src/app/list-your-property/page.tsx
+++ src/app/list-your-property/page.tsx
@@ -1,28 +1,42 @@
 import { PageTop } from "@/modules/layout";
-import { getTranslations } from "next-intl/server";
 import { Paragraph, Heading1, Heading2 } from "@/modules/typography";
+import { getServerAuthSession } from "@/server/auth";
+import Link from "next/link";
 
 const Page = async () => {
-  const t = await getTranslations("Start-Renting");
+  const u = await getServerAuthSession();
   return (
     <main>
       <PageTop>
-        <Heading1>{t("title")}</Heading1>
+        <Heading1>List your property</Heading1>
         <Paragraph className="mx-auto mt-4 max-w-3xl text-lg">
-          {t("description")}
+          Interested in listing your property? Here are all the needed steps
         </Paragraph>
       </PageTop>
       <div className="container flex flex-col gap-5">
         <section className="mt-5 flex flex-col gap-10">
           <article>
-            <Heading2>{t("What-Do-I-Need.title")}</Heading2>
-            <Paragraph>{t("What-Do-I-Need.description")}</Paragraph>
+            <Heading2>What do I need?</Heading2>
+            <Paragraph>
+              To list properties on our platform, you need to verify yourself on
+              of our checkpoints. First, apply for verification. Afterwards, you
+              can verify on the checkpoints Make sure to bring your ID, so we
+              can verify your identity.
+            </Paragraph>
           </article>
           <article>
-            <Heading2>{t("What-To-Do-After.title")}</Heading2>
-            <Paragraph>{t("What-To-Do-After.description")}</Paragraph>
+            <Heading2>What to do after?</Heading2>
+            <Paragraph>
+              Right after you are verified, you can list your property!
+            </Paragraph>
           </article>
         </section>
+        {u?.user.verified_status === "VERIFIED" ? (
+          <p>
+            It seems like you are verified, go ahead, make that listing!{" "}
+            <Link href="/listing/new">Start here</Link>
+          </p>
+        ) : null}
       </div>
     </main>
   );
