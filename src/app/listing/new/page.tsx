ef99a1feadbad43a7537a71ff71c7b0ebdb19819
mirekbartunek <mucus.hooide@gmail.com>
2025-03-15 09:54:01 +0100

    removed translations pt 2

--- src/app/listing/new/page.tsx
+++ src/app/listing/new/page.tsx
@@ -1,41 +1,38 @@
 import { ErrorPage, PageTop } from "@/modules/layout";
 import { db, users } from "@/server/db";
 import { eq } from "drizzle-orm";
-import { getTranslations } from "next-intl/server";
 import { getServerAuthSession } from "@/server/auth";
 import { NewNewListingForm } from "@/modules/listings/components/NewNewListingForm/NewNewListingForm";
 const Page = async () => {
   const session = await getServerAuthSession();
-  const unauthenticatedTranslations = await getTranslations("Unauthenticated");
-  if (!session?.user?.id)
+  if (!session?.user.id) {
     return (
       <main>
         <ErrorPage
-          title={unauthenticatedTranslations("title")}
-          desc={unauthenticatedTranslations("description")}
+          title="Unauthenticated"
+          desc="You must be authenticated to access this page"
         />
       </main>
     );
+  }
   const dbUser = await db.query.users.findFirst({
     where: eq(users.id, session.user.id),
   });
   const role = dbUser?.verified_status;
-  const unauthorizedTranslations = await getTranslations("Unauthorized");
   if (role === "UNVERIFIED")
     return (
       <main>
         <ErrorPage
-          title={unauthorizedTranslations("title")}
-          desc={unauthorizedTranslations("description")}
+          title="Unauthorized"
+          desc="You are not authorized to view this page"
         />
       </main>
     );
-  const t = await getTranslations("Create-a-new-listing");
   return (
     <main>
       <PageTop>
         <h1 className="text-4xl font-bold tracking-tight text-rose-500 md:text-5xl lg:text-6xl">
-          {t("title")}
+          Create a new listing1
         </h1>
       </PageTop>
       <section className="container">
